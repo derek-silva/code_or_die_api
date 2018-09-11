c8ed0ba2c734090db29b416c78b68cacf74c5f3f
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-11 12:59:45 -0500

    devise framework added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 	has_many :user_meet_ups
 	has_many :meet_ups, through: :user_meet_ups
 
