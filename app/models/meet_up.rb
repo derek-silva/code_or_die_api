6b01b46afca2e535623c79c67121c2cb1c83b628
Kenny Yang <kennyyang19@gmail.com>
2018-09-11 10:49:14 -0500

    reformatted database

--- app/models/meet_up.rb
+++ app/models/meet_up.rb
@@ -2,6 +2,6 @@ class MeetUp < ApplicationRecord
 	has_many :user_meet_ups
 	has_many :users, through: :user_meet_ups
 
-	validates :name, :topic, :address, :time, presence: true
+	validates :name, :topic, :address, :description, :date, :start_time, :end_time, presence: true
 
 end
